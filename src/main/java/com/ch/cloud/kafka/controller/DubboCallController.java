e481c4534774c154d4d184cdd95a5998cf959f10
麻智敏 <ZhiMinMa@sf-express.com>
2020-06-19 13:26:59 +0800

    Dubbo泛化调用

--- src/main/java/com/ch/cloud/kafka/controller/DubboCallController.java
+++ src/main/java/com/ch/cloud/kafka/controller/DubboCallController.java
@@ -1,13 +1,11 @@
 package com.ch.cloud.kafka.controller;
 
 import com.ch.Constants;
-import com.ch.cloud.kafka.pojo.DubboCall;
 import com.ch.cloud.kafka.pojo.DubboCall2;
 import com.ch.cloud.kafka.utils.DubboCallUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -21,21 +19,10 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class DubboCallController {
 
-    @Value("${share.path.libs}")
-    private String libsDir;
 
-/*
     @ApiOperation(value = "Dubbo 调用")
     @PostMapping("call")
-    public Object search(@RequestBody DubboCall record,
-                            @RequestHeader(Constants.TOKEN_USER) String username) {
-        return DubboCallUtils.invoke(record.getInterfaceName(), record.getMethod(), record.getParam(), record.getAddress(), record.getVersion());
-    }
-*/
-
-    @ApiOperation(value = "Dubbo 调用2")
-    @PostMapping("call2")
-    public Object call2(@RequestBody DubboCall2 record,
+    public Object call(@RequestBody DubboCall2 record,
                          @RequestHeader(Constants.TOKEN_USER) String username) {
         return DubboCallUtils.invoke(record.getAddress(), record.getVersion(),record.getInterfaceName(),
                 record.getMethod(), record.getParamClassName(),
