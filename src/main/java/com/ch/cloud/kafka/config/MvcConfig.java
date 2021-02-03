4744288f87eea3f8c7ffc1e16f23755fb573ad09
麻智敏 <ZhiMinMa@sf-express.com>
2021-02-03 18:59:24 +0800

    kafka user context util

--- src/main/java/com/ch/cloud/kafka/config/MvcConfig.java
+++ src/main/java/com/ch/cloud/kafka/config/MvcConfig.java
@@ -1,16 +1,25 @@
 package com.ch.cloud.kafka.config;
 
+import com.ch.Constants;
+import com.ch.cloud.kafka.utils.ContextUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Configuration
 @EnableWebMvc
+@Slf4j
 public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -21,4 +30,16 @@ public class MvcConfig implements WebMvcConfigurer {
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new HandlerInterceptor() {
+            @Override
+            public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+                String user = request.getHeader(Constants.TOKEN_USER);
+                log.info("filter user: {}", user);
+                ContextUtil.setUser(user);
+                return true;
+            }
+        }).addPathPatterns("/**");
+    }
 }
