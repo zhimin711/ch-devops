33648db526066f54595e0d3fae8a7aa80b9fe48a
mazhimin <mazhimin@12301.cc>
2022-03-29 18:27:44 +0800

    调整Kafka dependency and admin

--- src/main/java/com/ch/cloud/kafka/tools/ZkTopicUtils.java
+++ src/main/java/com/ch/cloud/kafka/tools/ZkTopicUtils.java
@@ -6,29 +6,29 @@ import com.ch.cloud.kafka.utils.KafkaSerializeUtils;
 import com.ch.utils.CommonUtils;
 import com.google.common.collect.Lists;
 import kafka.admin.AdminUtils;
+import kafka.admin.RackAwareMode;
 import kafka.admin.TopicCommand;
-import kafka.api.TopicMetadata;
+import kafka.server.ConfigType;
 import kafka.utils.ZkUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.I0Itec.zkclient.ZkClient;
-import scala.Function1;
-import scala.Tuple2;
-import scala.collection.Iterator;
+import org.I0Itec.zkclient.ZkConnection;
+import org.apache.kafka.common.security.JaasUtils;
+import scala.collection.JavaConversions;
 import scala.collection.Map;
 import scala.collection.Seq;
-import scala.collection.Set;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Properties;
+import java.util.stream.Collectors;
 
 /**
  * @author zhimin.ma
  * @date 2018/9/19 16:36
  */
 @Slf4j
-public class TopicManager {
+public class ZkTopicUtils {
 
     /*
      * 创建主题(注：创建不成功)
@@ -36,22 +36,16 @@ public class TopicManager {
      * --topic kafka-action --replication-factor 2 --partitions 3
      */
     public static void createTopic(TopicConfig config) {
-        ZkClient zkClient = null;
+        ZkUtils zkUtils = null;
         try {
-            zkClient = new ZkClient(config.getZookeeper());
-/*            if (!AdminUtils.topicExists(zkClient, config.getTopicName())) {
-                AdminUtils.createTopic(zkClient, config.getTopicName(), config.getPartitions(),
-                        config.getReplicationFactor(), config.getProperties());
-
-                log.info("{}:successful create!", config.getTopicName());
-            } else {
-                log.error(config.getTopicName() + " is exits!");
-            }*/
-
+            zkUtils = ZkUtils.apply(config.getZookeeper(), 30000, 30000, JaasUtils.isZkSaslEnabled());
+            // 创建一个单分区单副本名为t1的topic
+            AdminUtils.createTopic(zkUtils, config.getTopicName(), config.getPartitions(), config.getReplicationFactor(), config.getProperties(), RackAwareMode.Enforced$.MODULE$);
+            zkUtils.close();
         } catch (Exception e) {
             log.error("zk connect or topic create error!", e);
         } finally {
-            close(zkClient);
+            close(zkUtils);
         }
     }
 
@@ -68,19 +62,57 @@ public class TopicManager {
         if (!config.getProperties().isEmpty()) {
             config.getProperties().forEach((k, v) -> configs.add(" --config " + k + "=" + v));
         }
-        String s = "--zookeeper 10.203.248.126:2181,10.203.248.127:2181,10.203.248.128:2181,10.203.248.129:2181,10.203.248.130:2181/kafka/eostbpcore" +
-                " --create --topic SHIVA_TRTMS_GROUND_PLAN_ORDER_NOTIFY2" +
-                " --partitions 3 --replication-factor 1" +
-                " --config max.message.bytes=204800 --config flush.messages=2";
-        String s1 = "--zookeeper " + config.getZookeeper() +
-                " --create --topic " + config.getTopicName() +
-                " --partitions " + config.getPartitions() + " --replication-factor " + config.getReplicationFactor();
+        String s = "--zookeeper 10.203.248.126:2181,10.203.248.127:2181,10.203.248.128:2181,10.203.248.129:2181,10.203.248.130:2181/kafka/eostbpcore" + " --create --topic SHIVA_TRTMS_GROUND_PLAN_ORDER_NOTIFY2" + " --partitions 3 --replication-factor 1" + " --config max.message.bytes=204800 --config flush.messages=2";
+        String s1 = "--zookeeper " + config.getZookeeper() + " --create --topic " + config.getTopicName() + " --partitions " + config.getPartitions() + " --replication-factor " + config.getReplicationFactor();
         if (!configs.isEmpty()) {
             s1 += String.join(" ", configs);
         }
         String[] args = s1.split(" ");
         log.info(Arrays.toString(args));
         TopicCommand.main(args);
+    }
+
+    /**
+     * 除某主题
+     * kafka-topics.sh --zookeeper localhost:2181 --topic kafka-action --delete
+     */
+    private static void deleteTopic(TopicConfig config) {
+        deleteTopic(config.getZookeeper(), config.getTopicName());
+    }
+
+    /*
+     *删除某主题
+     *kafka-topics.sh --zookeeper localhost:2181 --topic kafka-action --delete
+     */
+    public static void deleteTopic(String zkUrl, String topic) {
+        ZkUtils zkUtils = null;
+        try {
+            zkUtils = ZkUtils.apply(zkUrl, 30000, 30000, JaasUtils.isZkSaslEnabled());
+            // 删除topic 't1'
+            AdminUtils.deleteTopic(zkUtils, topic);
+            zkUtils.close();
+        } catch (Exception e) {
+            log.error("delete error! => " + topic, e);
+        } finally {
+            close(zkUtils);
+        }
+    }
+
+    /**
+     * 修改主题配置     kafka-config --zookeeper localhost:2181 --entity-type topics --entity-name kafka-action
+     * --alter --add-config max.message.bytes=202480 --alter --delete-config flush.messages
+     */
+    private static void updateTopic(TopicConfig config) {
+        ZkUtils zkUtils = ZkUtils.apply(config.getZookeeper(), 30000, 30000, JaasUtils.isZkSaslEnabled());
+        Properties props = AdminUtils.fetchEntityConfig(zkUtils, ConfigType.Topic(), config.getTopicName());
+        // 增加topic级别属性
+//        props.put("min.cleanable.dirty.ratio", "0.3");
+        // 删除topic级别属性
+//        props.remove("max.message.bytes");
+        // 修改topic 'test'的属性
+        props.putAll(config.getProperties());
+        AdminUtils.changeTopicConfig(zkUtils, config.getTopicName(), props);
+        zkUtils.close();
     }
 
     /*
@@ -96,97 +128,52 @@ public class TopicManager {
      *kafka-topics.sh --zookeeper localhost:2181 --list
      */
     public static List<String> getTopicsByName(String zkUrl, String topicName) {
-        ZkClient zkClient = null;
-        List<String> topics = Lists.newArrayList();
+        ZkUtils zkUtils = null;
         try {
-            zkClient = new ZkClient(zkUrl);
-            /*Seq<String> topicSeq = ZkUtils.getAllTopics(zkClient);
-            Iterator<String> iterator = topicSeq.iterator();
-            while (iterator.hasNext()) {
-                String topic = iterator.next();
-                log.debug("topic: {}", topic);
-                if (CommonUtils.isNotEmpty(topicName)) {
-                    if (topic.contains(topicName)) topics.add(topic);
-                } else {
-                    topics.add(topic);
-                }
-            }*/
+            zkUtils = ZkUtils.apply(zkUrl, 30000, 30000, JaasUtils.isZkSaslEnabled());
+            List<String> topics = JavaConversions.seqAsJavaList(zkUtils.getAllTopics());
+            return topics.stream().filter(e -> CommonUtils.isEmpty(topicName) || e.contains(topicName)).collect(Collectors.toList());
         } catch (Exception e) {
-            log.error("zk connect or fetch topics error!" + topics, e);
+            log.error("zk connect or fetch topics error!" + topicName, e);
         } finally {
-            close(zkClient);
+            close(zkUtils);
         }
-        return topics;
+        return Lists.newArrayList();
     }
 
-    /**
-     * 修改主题配置
-     * kafka-config --zookeeper localhost:2181 --entity-type topics --entity-name kafka-action
-     * --alter --add-config max.message.bytes=202480 --alter --delete-config flush.messages
-     */
-    public static void alterTopicConfig(String zkUrl, String topicName, Properties properties) {
-        ZkClient zkClient = null;
-        try {
-            zkClient = new ZkClient(zkUrl);
-            zkClient.setZkSerializer(KafkaSerializeUtils.jsonZk());
-            //先取得原始的参数，然后添加新的参数同时去除需要去除的参数
-/*            Properties oldProperties = AdminUtils.fetchTopicConfig(zkClient, topicName);
-            if (!oldProperties.isEmpty()) {
-                properties.putAll(new HashMap<>(oldProperties));
-            }*/
-            // 增加topic级别属性
-            properties.put("min.cleanable.dirty.ratio", "0.3");
-            // 删除topic级别属性
-            properties.remove("max.message.bytes");
-//            AdminUtils.changeTopicConfig(zkClient, topicName, properties);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            close(zkClient);
-        }
-    }
-
-    /*
-     *删除某主题
-     *kafka-topics.sh --zookeeper localhost:2181 --topic kafka-action --delete
-     */
-    public static void deleteTopic(String zkUrl, String topic) {
-        ZkClient zkClient = null;
-        try {
-            zkClient = new ZkClient(zkUrl);
-//            AdminUtils.deleteTopic(zkClient, topic);
-        } catch (Exception e) {
-            log.error("delete error! => " + topic, e);
-        } finally {
-            close(zkClient);
-        }
-    }
 
     /**
      * 得到所有topic的配置信息
      * kafka-configs.sh --zookeeper localhost:2181 --entity-type topics --describe
      */
     public static void listTopicAllConfig(String zkUrl) {
-        ZkClient zkClient = null;
+        ZkUtils zkUtils = null;
         try {
-            zkClient = new ZkClient(zkUrl);
-
-         /*   Map<String, Properties> configsMap = AdminUtils.fetchAllTopicConfigs(zkClient);
-            Iterator<Tuple2<String, Properties>> iterator = configsMap.iterator();
-            while (iterator.hasNext()) {
-                Tuple2<String, Properties> tuple2 = iterator.next();
-                System.out.println(tuple2._1 + tuple2._2);
-            }*/
+            zkUtils = ZkUtils.apply(zkUrl, 30000, 30000, JaasUtils.isZkSaslEnabled());
+            java.util.Map<String, Properties> configs = JavaConversions.mapAsJavaMap(AdminUtils.fetchAllTopicConfigs(zkUtils));
+            // 获取特定topic的元数据
+//            MetadataResponse.TopicMetadata topicMetadata = AdminUtils.fetchTopicMetadataFromZk("topic-19",zkUtils);
+            // 获取特定topic的配置信息
+//            Properties properties = AdminUtils.fetchEntityConfig(zkUtils,"topics","kafka-test");
+//            for (Map.Entry<String, Properties> entry : configs.entrySet()) {
+//                System.out.println("key=" + entry.getKey() + " ;value= " + entry.getValue());
+//            }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            close(zkClient);
+            close(zkUtils);
         }
     }
 
     private static void close(ZkClient zkClient) {
         if (zkClient != null) {
             zkClient.close();
+        }
+    }
+
+    private static void close(ZkUtils zkUtils) {
+        if (zkUtils != null) {
+            zkUtils.close();
         }
     }
 
@@ -216,13 +203,19 @@ public class TopicManager {
 
     public static List<TopicInfo> getTopics(String zkUrl) {
         ZkClient zkClient = null;
+        ZkConnection zkConnection = null;
         List<TopicInfo> topics = Lists.newArrayList();
         try {
             zkClient = new ZkClient(zkUrl, 360000);
+            zkConnection = new ZkConnection(zkUrl, 360000);
             zkClient.setZkSerializer(KafkaSerializeUtils.jsonZk());
-      /*      Seq<String> topicSeq = ZkUtils.getAllTopics(zkClient);
-            Set<TopicMetadata> topicMetadataSet = AdminUtils.fetchTopicMetadataFromZk(topicSeq.toSet(), zkClient);
+            ZkUtils zkUtils = new ZkUtils(zkClient, zkConnection, false);
+            Seq<String> topicSeq = zkUtils.getAllTopics();
+            Map<String, Properties> config = AdminUtils.fetchAllTopicConfigs(zkUtils);
 
+            java.util.Map<String, Properties> configs = JavaConversions.mapAsJavaMap(config);
+            System.out.println(configs);
+/*
             Iterator<TopicMetadata> metadataIterator = topicMetadataSet.iterator();
             while (metadataIterator.hasNext()) {
                 kafka.javaapi.TopicMetadata meta = new kafka.javaapi.TopicMetadata(metadataIterator.next());
@@ -233,8 +226,19 @@ public class TopicManager {
             log.error("zk connect or fetch topics error!");
         } finally {
             close(zkClient);
+            close(zkConnection);
         }
         return topics;
+    }
+
+    private static void close(ZkConnection zkConnection) {
+        if (zkConnection != null) {
+            try {
+                zkConnection.close();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private static TopicInfo convertTopicMeta(kafka.javaapi.TopicMetadata meta, String topic) {
