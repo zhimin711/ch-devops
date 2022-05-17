4e27d8cfde31c65483a0938b5df9cd09ecf6737f
仙_梦如尘 <zhimin711@sina.com>
2022-05-17 23:49:09 +0800

    modify nacos service detail get

--- dao/src/main/java/com/ch/cloud/nacos/dto/ServiceDetailDTO.java
+++ dao/src/main/java/com/ch/cloud/nacos/dto/ServiceDetailDTO.java
@@ -1,6 +1,6 @@
 package com.ch.cloud.nacos.dto;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import lombok.Data;
 
 import java.util.List;
@@ -14,8 +14,8 @@ import java.util.List;
 @Data
 public class ServiceDetailDTO {
 
-    private JSON service;
+    private JSONObject service;
 
-    private List<?> clusters;
+    private List<JSONObject> clusters;
 
 }
